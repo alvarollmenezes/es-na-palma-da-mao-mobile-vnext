f8247e182b0d96a3386b10450921841c61132162
Gary M. Silva <garymsilva@outlook.com>
2019-06-28 20:43:01 -0300

    refactor(my-services): quantidade de módulos dinâmica

--- src/app/navegacao/pages/my-services/my-services.ts
+++ src/app/navegacao/pages/my-services/my-services.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, App, NavParams } from 'ionic-angular';
 import { AuthQuery, AuthNeededService } from '@espm/core';
 
@@ -21,28 +21,40 @@ type Favorite = {
   templateUrl: 'my-services.html',
 })
 export class MyServicesPage{
-    
-    private servicosSelecionados:Array<Favorite>;
-    private serveSelect:Array<Favorite[]> = [];
-   
-    constructor(
+  @ViewChild('slideswrapper') slidesWrapper: any;
+  private servicosSelecionados:Array<Favorite>;
+  private serveSelect:Array<Favorite[]> = [];
+  
+  
+  constructor(
     protected appCtrl: App,
     protected authQuery: AuthQuery,
     protected authNeeded: AuthNeededService,
     protected navCtrl: NavController,
     private navParams: NavParams) {
-
-      this.servicosSelecionados = this.navParams.data; 
-
-      this.servicosSelecionados.map((elemento: Favorite, index: number) => {
-        if (index%4 === 0) {
-          this.serveSelect.push([])
-        }
-        this.serveSelect[this.serveSelect.length-1].push(elemento);
-      });
-      
-      
+    
+    this.servicosSelecionados = this.navParams.data; 
   }
+
+  ionViewDidEnter() {
+    this.serveSelect.splice(0, this.serveSelect.length);
+    this.servicosSelecionados.map((elemento: Favorite, index: number) => {
+      if (index % this.itemsPerPage() === 0) {
+        this.serveSelect.push([])
+      }
+      this.serveSelect[this.serveSelect.length-1].push(elemento);
+    });
+  }
+
+  itemsPerPage(): number {
+    console.log(this.slidesWrapper);
+    let list = this.slidesWrapper.nativeElement;
+    console.log(`${list.clientWidth} / ${list.clientHeight}`);
+    let rows = Math.floor((list.clientHeight - 40) / 140);
+    let columns = Math.floor((list.clientWidth - 10) / 140);
+    return rows * columns;
+  }
+
   openPage = (page: string, accessDenied: boolean = false) => {
     if (accessDenied) {
       this.authNeeded.showAuthNeededModal();
@@ -54,3 +66,4 @@ export class MyServicesPage{
     this.navCtrl.push('SelectFavoritePage')
   }
 }
+  
